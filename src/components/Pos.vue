f8a81c83cbabf704573edee243963c48cc07566c
wkkwll <15632712156@163.com>
2018-07-19 11:30:00 +0800

    <feat>(<all>) : <test>
    <body>
    
    <footer>

---
+++ src/components/Pos.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pos">
+      hello vue
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pos'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
